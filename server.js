09ccc3e0da1fb0a727b0500b5197a27603e1fda9
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2025-02-17 22:57:31 +0900

    map 1차

--- server.js
+++ server.js
@@ -14,38 +14,6 @@ app.use(cors());
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname)));
 
-//
-app.get('/api/geocode', async (req, res) => {
-    const address = req.query.address;
-
-    if (!address) {
-        return res.status(400).json({ error: "주소를 입력하세요." });
-    }
-
-    const clientId = process.env.MAPCLIENTID;
-    const clientSecret = process.env.MAPSCRETKEY;
-    const encodedAddress = encodeURIComponent(address);
-
-    const url = `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${encodedAddress}`;
-
-    try {
-        const response = await fetch(url, {
-            method: "GET",
-            headers: {
-                "x-ncp-apigw-api-key-id": clientId,
-                "x-ncp-apigw-api-key": clientSecret,
-                'Accept': 'application/json'
-            }
-        });
-
-        const data = await response.json();
-        res.json(data);
-
-    } catch (error) {
-        console.error("Geocoding API 오류:", error);
-        res.status(500).json({ error: "서버 오류" });
-    }
-});
 
 // 서버 실행
 app.listen(port, () => {
@@ -73,11 +41,9 @@ app.get("/map/list/detail", (req, res) => {
     // 필터링하여 해당 district에 속하는 데이터만 반환
     const filteredList = dummyData.filter(item => item.district == district);
 
-
     if (filteredList.length === 0) {
-        return res.status(404).json({ message: "해당 지역의 데이터가 없습니다." });
+        return res.status(404).json({ message: "해당 지역의 데이터가 없습니다."});
     }
-
     res.send(filteredList);
 });
 
